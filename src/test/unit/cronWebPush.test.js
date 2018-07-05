797910c2ba72b391a51f6123fd519d7e38a9367d
Mildnchrt <Nutcharueta.s@ku.th>
2018-07-05 16:23:31 +0700

    Created unit test folder"

---
+++ src/test/unit/cronWebPush.test.js
@@ -0,0 +1,56 @@
+const { webPushNotification } = require('../../services/web-noti')
+const libFirestore  = require('../../library/firestore')
+// const sellsuki = require('../../library/sellsuki')
+// sellsuki.getUser = jest.fn().mockReturnValue({data: {results: {}}})
+
+
+
+describe('describe webPushnotification endpoint', () => {
+  it('getStage', () => {
+    console.log('check user are in product stage')
+    let user = {
+      count_product: 1
+    }
+    expect(webPushNotification.getStage(user)).toEqual('product')
+  })
+
+  it('sent null data to transferData', () => {
+    let output = {
+      storeId: '',
+      playerId: '',
+      isAllow: false,
+      isComplete: false,
+      stage: 'product',
+      creatAt: '',
+      updateAt: '',
+      dataOneSignal: {},
+      dataSellsuki: {}
+    }
+    expect(webPushNotification.transferData('', '', null, null, '', '', '', null, null)).toEqual(output)
+  })
+
+  it('sent data to transferData', () => {
+    let data = webPushNotification.transferData('01', '01', false, false, '', 'today', '', {test: 'test'}, {test: 'test'})
+
+    let output = {
+      storeId: '01',
+      playerId: '01',
+      isAllow: false,
+      isComplete: false,
+      stage: 'product',
+      creatAt: 'today',
+      updateAt: '',
+      dataOneSignal: {test: 'test'},
+      dataSellsuki: {test: 'test'}
+    }
+    expect(data).toEqual(output)
+  })
+  // it.only('getUserFromSellsuki', async () => {
+  //   // test.getActiveUser = jest.fn().mockReturnValue(true)
+  //   // sellsuki.getUser = jest.fn().mockReturnValue({data: {results: {}}})
+  //   await webPushNotification.getUserFromSellsuki('1')
+
+  //   // console.log('mock', sellsuki.getUser)
+  //   expect(sellsuki.getUser.mock.calls.length).toBe(1)
+  // })
+})

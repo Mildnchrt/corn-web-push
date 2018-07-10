53c19f28226184a7399573eb756ba7d534f266c0
Mildnchrt <Nutcharueta.s@ku.th>
2018-07-10 14:19:56 +0700

    Delete consolelog and comment

--- src/test/unit/controllersTest/cronWebPush.test.js
+++ src/test/unit/controllersTest/cronWebPush.test.js
@@ -5,8 +5,6 @@ const userNotDone = [
   {storeId: '1'}, 
   {storeId: '2'}
 ]
-webPushNotification.getUserNotComplete = jest.fn().mockReturnValue(userNotDone)
-
 let dataStore = {
   storeIds: '1,2', 
   data: [
@@ -14,8 +12,6 @@ let dataStore = {
     {}
   ]
 }
-webPushNotification.setDataStoreCollections = jest.fn().mockReturnValue(dataStore)
-
 const userSellsuki = {
   results: [
     {
@@ -34,21 +30,22 @@ const userSellsuki = {
     }
   ]
 }
+
+webPushNotification.getUserNotComplete = jest.fn().mockReturnValue(userNotDone)
+webPushNotification.setDataStoreCollections = jest.fn().mockReturnValue(dataStore)
 webPushNotification.getUserFromSellsuki = jest.fn().mockReturnValue(userSellsuki)
-// --- mock function calling count services 
 webPushNotification.updateDataToFirestore = jest.fn()
 webPushNotification.getUserStage = jest.fn()
 webPushNotification.pushNotification = jest.fn()
 
 const { cronController } = require('../../../controllers/web-noti')
+
 describe('describe cronWebPush endpoint', () => {
   it ('cronWebPush', async () => {
     let res = await cronController()
-    console.log('res >>> ' , res)
     expect(webPushNotification.getUserNotComplete.mock.calls.length).toBe(2)
     expect(webPushNotification.setDataStoreCollections.mock.calls.length).toBe(1)
     expect(webPushNotification.getUserFromSellsuki.mock.calls.length).toBe(1)
-    // --- 
     expect(webPushNotification.getUserStage.mock.calls.length).toBe(2)
     expect(webPushNotification.pushNotification.mock.calls.length).toBe(2)
     expect(res).toEqual({
